daec2e38eeeaa44c6244786c593c4d55f82a0ee2
Bishal Raj Parajuli <bishal.parajuli1312@gmail.com>
2025-04-18 03:43:46 +1000

    feat(api): add Fastify static support and Swagger integration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,12 +4,25 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { generateOpenApi } from '@ts-rest/open-api';
+import { contract } from '@mytechstack/ts-rest';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const document = generateOpenApi(contract, {
+    info: {
+      title: 'Posts API',
+      version: '1.0.0',
+    },
+  });
+
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
